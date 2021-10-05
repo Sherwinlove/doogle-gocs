48918f51fa208eeabb54434c8a362f6ac3d370db
Sherwinlove <ilak0603@gmail.com>
2021-10-04 21:07:33 -0700

    added styling labels for collection of docs

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,15 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className="bg-white px-10 md:px-0">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+          <div className="flex items-center justify-between pb-5">
+            <h3 className="font-medium flex-grow">My Documents</h3>
+            <p className="mr-12">Date Created</p>
+            <Icon name="folder" size="3xl" color="gray" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
